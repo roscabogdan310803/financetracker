51995d325b014fb0bb684288d5835dd35e96c487
unknown <rnbrosca2003@gmail.com>
2024-07-15 19:09:30 +0300

    first commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,15 +2,22 @@ import express, { Express } from "express";
 import mongoose from "mongoose";
 import financialRecordRouter from "./routes/financial-records";
 import cors from "cors";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
+const mongoURI = process.env.MONGODB_URI;
 
 app.use(express.json());
 app.use(cors());
 
-const mongoURI: string =
-  "VITE_MONG";
+if (!mongoURI) {
+  throw new Error("Please define the MONGODB_URI environment variable in the .env file");
+}
+
 mongoose
   .connect(mongoURI)
   .then(() => console.log("CONNECTED TO MONGODB!"))
