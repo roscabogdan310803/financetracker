0c058f16cb0247955d7b28c78b76fa9f15665fd1
unknown <rnbrosca2003@gmail.com>
2024-07-15 20:16:40 +0300

    .

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,21 +2,15 @@ import express, { Express } from "express";
 import mongoose from "mongoose";
 import financialRecordRouter from "./routes/financial-records";
 import cors from "cors";
-import dotenv from "dotenv";
-
-// Load environment variables from .env file
-dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
-const mongoURI = "mongodb+srv://ibacarim:MANAlow32@financetracker.tobrihe.mongodb.net/"
 
 app.use(express.json());
 app.use(cors());
 
-if (!mongoURI) {
-  throw new Error("Please define the MONGODB_URI environment variable in the .env file");
-}
+const mongoURI: string =
+"mongodb+srv://ibacarim:MANAlow32@financetracker.tobrihe.mongodb.net/";
 
 mongoose
   .connect(mongoURI)
