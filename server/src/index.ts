24a2cb80e299411c9da2977b5d6a7c10eba7ca0d
unknown <rnbrosca2003@gmail.com>
2026-02-02 15:56:41 +0200

    .

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,33 +1,16 @@
-import express, { Express } from "express";
-import mongoose from "mongoose";
-import financialRecordRouter from "./routes/financial-records";
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-// MongoDB Connection
-const mongoURI: string = process.env.MONGODB_URI || "";
+const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(mongoURI)
-  .then(() => console.log("✅ CONNECTED TO MONGODB!"))
-  .catch((err) => console.error("❌ Failed to Connect to MongoDB:", err));
-
-// Routes
-app.use("/financial-records", financialRecordRouter);
-
-// Health check endpoint
-app.get("/", (req, res) => {
-  res.send("Server is running! 🚀");
-});
-
-app.listen(port, () => {
-  console.log(`🚀 Server Running on Port ${port}`);
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
